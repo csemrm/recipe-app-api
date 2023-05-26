9db8d4f7fccd7e6a79c92ed7c4e1e37ece1e1465
Mostafizur Rahman <csemrm@gmail.com>
2023-05-25 23:20:33 -0700

    Recipe, Tag....

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,7 +4,7 @@ Django admin customization
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-from .models import User, Recipe
+from .models import User, Recipe, Tag
 
 
 class UserAdmin(BaseUserAdmin):
@@ -44,3 +44,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Recipe)
+admin.site.register(Tag)
